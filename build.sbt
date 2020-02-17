ca07880bb13ac0ea8c31a0701061cac4ca953191
cpchung <chakpongchung@gmail.com>
2020-02-17 18:22:07 -0500

    update

--- build.sbt
+++ build.sbt
@@ -6,3 +6,7 @@ scalaVersion := "2.13.1"
 
 // https://mvnrepository.com/artifact/com.typesafe.play/play-json
 libraryDependencies += "com.typesafe.play" %% "play-json" % "2.8.0"
+// https://mvnrepository.com/artifact/io.circe/circe-parser
+libraryDependencies += "io.circe" %% "circe-parser" % "0.13.0"
+libraryDependencies += "io.circe" %% "circe-core" % "0.13.0"
+libraryDependencies += "io.circe" %% "circe-generic" % "0.13.0"
---
+++ src/main/scala/catchThemAll/Move.scala
@@ -0,0 +1,36 @@
+package catchThemAll
+
+//case class Move(num: Int,
+//                accuracy: Either[Int, Boolean],
+//                basePower: Int,
+//                category: String,
+//                desc: Option[String],
+//                shortDesc: String,
+//                id: String,
+//                isNonstandard: Option[String],
+//                isViable: Option[Boolean],
+//                name: String,
+//                pp: Int,
+//                priority: Int,
+//                flags: Flags,
+//                secondary: Option[Either[Boolean, Secondary]])
+//
+//type SecondaryType = Either[Boolean,Secondary]
+//
+//case class Flags(authentic: Option[Int], bite: Option[Int])
+//
+//case class Secondary(chance: Option[Int],
+//                     volatileStatus: Option[String],
+//                     boosts: Option[Boosts],
+//                     self: Option[Self],
+//                     status: Option[String])
+//
+//case class Boosts(atk: Option[Int], `def`: Option[Int])
+//
+//case class Self(boosts: Option[Boosts])
+
+//object MoveSchema {
+//  implicit val format: Format[MoveSchema] = Json.format
+//}
+
+
