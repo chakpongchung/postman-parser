13f90b39b9a565f8e8c703919d58e65b24df580a
cpchung <chakpongchung@gmail.com>
2020-02-17 19:45:01 -0500

    done!

--- src/main/scala/catchThemAll/MoveParser.scala
+++ src/main/scala/catchThemAll/MoveParser.scala
@@ -6,102 +6,60 @@ import io.circe.generic.semiauto.deriveDecoder
 
 object MoveParser extends App {
 
-
-  case class Move(num: Int,
-                  accuracy: AccuracyType,
-                  basePower: Int,
-                  category: String,
-                  desc: Option[String],
-                  shortDesc: String,
-                  id: String,
-                  isNonstandard: Option[String],
-                  isViable: Option[Boolean],
-                  name: String,
-                  pp: Int,
-                  priority: Int,
-                  flags: Flags,
-                  secondary: Option[SecondaryType])
-
-
-  type AccuracyType = Either[Int, Boolean]
-  type SecondaryType = Either[Boolean, Secondary]
-
-  case class Flags(authentic: Option[Int], bite: Option[Int])
-
-  case class Secondary(chance: Option[Int],
-                       volatileStatus: Option[String],
-                       boosts: Option[Boosts],
-                       self: Option[Self],
-                       status: Option[String])
-
-  case class Boosts(atk: Option[Int], `def`: Option[Int])
-
-  case class Self(boosts: Option[Boosts])
+  implicit final val BoostsDecoder: Decoder[Boosts] = deriveDecoder
+  implicit final val selfDecoder: Decoder[Self] = deriveDecoder
+  implicit final val SecondaryDecoder: Decoder[Secondary] = deriveDecoder
+  implicit final val secondaryTypeDecoder: Decoder[Either[Boolean, Secondary]] = Decoder[Boolean] either Decoder[Secondary]
+  implicit final val AccuracyTypeDecoder: Decoder[Either[Int, Boolean]] = Decoder[Int] either Decoder[Boolean]
+  implicit final val flagsDecoder: Decoder[Flags] = deriveDecoder
+  implicit final val moveDecoder: Decoder[Move] = deriveDecoder
 
 
   val move_collections = "./resources/moves.json"
   val jsonifiedString = scala.io.Source.fromFile(move_collections).mkString
-
-  implicit final val BoostsDecoder: Decoder[Boosts] = deriveDecoder
-  implicit final val selfDecoder: Decoder[Self] = deriveDecoder
-
-  implicit final val SecondaryDecoder: Decoder[Secondary] = deriveDecoder
-  implicit final val secondaryTypeDecoder: Decoder[SecondaryType] = Decoder[Boolean] either Decoder[Secondary]
-
-  implicit final val AccuracyTypeDecoder: Decoder[AccuracyType] = Decoder[Int] either Decoder[Boolean]
-
-  implicit final val flagsDecoder: Decoder[Flags] = deriveDecoder
-  implicit final val moveDecoder: Decoder[Move] = deriveDecoder
 
   val result = io.circe.parser.decode[List[Move]](jsonifiedString)
   println(result)
 
   result match {
     case Right(x) => // compare your case class here
-
       //          println(x(0).category)
       //          println(x(0).secondary)
-
       for (i <- 0 until x.length) {
         println(s"$i is ${x(i)}")
         val move = x(i)
-
         move.secondary
         match {
           case Some(sec) =>
 
             sec match {
-              case secondaryType: SecondaryType =>
-                //                println(yy)
-                secondaryType match {
+              case Left(b) =>
+                println("secondary is boolean: " + b)
 
-                  case secondar: Secondary =>
-//                    secondar.boosts match {
-//                      case Some(boostsV) =>
-//
-//                        boostsV.atk match {
-//                          case Some(atkV) => println("atk: " + atkV)
-//                          case  None => println()
-//                        }
-//                    }
+              case Right(s) =>
+                s.boosts match {
 
+                  case Some(boosts) =>
+
+                    boosts.atk match {
+                      case Some(atk) => println("atk: " + atk)
+                      case None => println("boosts has no atk")
+                    }
+                  case None =>
+
+                    println("secondary has no boost")
                 }
-
-
             }
 
           case None =>
-            //            println("none")
-            println()
+            println("no secondary")
 
 
         }
       }
 
     case Left(err) =>
-      println(err)
-      println("none")
-    //      throw new Exception("couldnt parse: ", err)
+      throw new Exception("couldnt parse: ", err)
   }
 
 
